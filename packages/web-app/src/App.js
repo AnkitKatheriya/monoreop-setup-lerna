9749e3336ac41099b1b42bc72a12b3f3ee973a9c
Ankit Katheriya <ankit_katheriya@epam.com>
2022-04-06 21:09:19 +0530

    change for lerna changed command

--- packages/web-app/src/App.js
+++ packages/web-app/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h2>Web-app</h2>
         <p>Adding paragraph to test moduel build</p>
+        <h1>Adding to check lerna changed command</h1>
       </header>
     </div>
   );
